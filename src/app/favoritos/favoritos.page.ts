bc3903df2ff968bf30fd0eb843ccb711cad2ef36
Juan Pablo Sanchez <jpsanchez@inti-moda.com>
2020-01-24 17:57:50 -0500

    commit from work 2020-01-24 17:57

--- src/app/favoritos/favoritos.page.ts
+++ src/app/favoritos/favoritos.page.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritosPage implements OnInit {
   public form = [
-    { val: 'Pepperoni', isChecked: true },
-    { val: 'Sausage', isChecked: false },
-    { val: 'Mushroom', isChecked: false }
+    {
+      val: 'Mantemimiento de Usuarios',
+      isChecked: true
+    },
+    {
+      val: 'Cambio de Clave',
+      isChecked: true
+    },
+    {
+      val: 'Favoritos',
+      isChecked: true
+    },
+    {
+      val: 'Mantenimiento de Perfil',
+      isChecked: true
+    },
+    {
+      val: 'Permisos',
+      isChecked: true
+    },
+    {
+      val: 'Ambiente',
+      isChecked: true
+    },
+    {
+      val: 'Parametros FE',
+      isChecked: true
+    },
+    {
+      val: 'Procesar Dctos Electronicos',
+      isChecked: true
+    }
   ];
 
   constructor() { }
