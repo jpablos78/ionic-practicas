85dd9e4473f12c8e6acbd556769eaee15e6640fa
JPablos <jpablos2011.2@gmail.com>
2020-02-03 06:47:35 -0500

    commit from home 2020-02-03 06:47

--- src/app/procesar-documentos-electronicos/enviar-mail/enviar-mail.page.ts
+++ src/app/procesar-documentos-electronicos/enviar-mail/enviar-mail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-enviar-mail',
@@ -7,9 +9,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnviarMailPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public loadingController: LoadingController,
+    public alertController: AlertController
+  ) { }
 
   ngOnInit() {
   }
+  onClickBtnAgregarMail() {
+    this.presentAlertPrompt();
+  }
 
+  onClickBtnEnviarMail() {
+    this.presentLoading();
+  }
+
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      message: 'Procesando ...',
+      spinner: 'bubbles',
+      duration: 2000
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+
+    console.log('Loading dismissed!');
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Ingrese el nombre'
+        },
+        {
+          name: 'name2',
+          type: 'email',
+          id: 'name2-id',
+          placeholder: 'Ingrese el correo'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
